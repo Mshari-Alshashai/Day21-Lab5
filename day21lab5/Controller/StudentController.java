bef9ec3af32a93758170389b665cde177933576b
Mshari-Alshashai <msharialshashai@gmail.com>
2024-11-17 13:26:55 +0300

    Update StudentController.java

--- day21lab5/Controller/StudentController.java
+++ day21lab5/Controller/StudentController.java
@@ -10,11 +10,7 @@ import java.util.ArrayList;
 @RequestMapping("/api/v1/student")
 public class StudentController {
     ArrayList<Student> students = new ArrayList<>();
-    ArrayList<Student> firstHonors = new ArrayList<>();
-    ArrayList<Student> secondHonors = new ArrayList<>();
-    ArrayList<Student> thirdHonors = new ArrayList<>();
-
-
+    
     @GetMapping("/display")
     public ArrayList<Student> getStudents() {
         return students;
@@ -39,13 +35,11 @@ public class StudentController {
     }
 
     @PostMapping("/honor")
-    public ApiResponse honor(){
-            for (Student student : students) {
-                if (student.getGPA()>3.75)firstHonors.add(student);
-                else if (student.getGPA()>3.5)secondHonors.add(student);
-                else if (student.getGPA()>3)thirdHonors.add(student);
-            }
-            return new ApiResponse("Successfully categorized students");
+    public ApiResponse honor(@RequestBody Student student) {
+                if (student.getGPA()>3.75)return new ApiResponse("First Honor Student");
+                else if (student.getGPA()>3.5)return new ApiResponse("Second Honor Student");
+                else if (student.getGPA()>3)return new ApiResponse("Third Honor Student");
+            return new ApiResponse("Not an honor student");
     }
 
     @GetMapping("/good-students")
